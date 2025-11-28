120c01b868d4eb86be22371df52c4c5489c9a41e
shriramhalyal2000 <shriramhalyal40@gmail.com>
2025-11-28 16:11:59 +0530

    updates on frntend backend

--- DevOps_Internship_Assignments/crud-dd-task-mean-app/crud-dd-task-mean-app/frontend/src/app/services/tutorial.service.ts
+++ DevOps_Internship_Assignments/crud-dd-task-mean-app/crud-dd-task-mean-app/frontend/src/app/services/tutorial.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://54.236.201.80:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
